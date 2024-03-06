8c007d4ce7ec84727918cccadc03e82a9c0f6654
KOPRO-WORK-2\kopro <michael@koprodo.de>
2024-03-06 17:13:26 +0100

    added rnbo synth device

--- p5/loadRNBO.js
+++ p5/loadRNBO.js
@@ -1,7 +1,9 @@
 
 
 let context;
-let device;
+let drumDevice;
+let synthDevice;
+
 let gainNode;
 let audioStarted = false;
 
@@ -9,13 +11,21 @@ async function loadRnbo( ){
 
   context = new AudioContext();
 
-  let rawPatcher = await fetch("export/drums.export.json");
-  let patcher = await rawPatcher.json();
-  device = await RNBO.createDevice({ context, patcher });
+  let drumsPatcher = await fetch("export/drums/drums.export.json");
+  let patcher = await drumsPatcher.json();
+  drumDevice = await RNBO.createDevice({ context, patcher });
+
+  let synthPatcher = await fetch("export/synth/synth.export.json");
+  patcher = await synthPatcher.json();
+  synthDevice = await RNBO.createDevice({ context, patcher });
   
   gainNode = context.createGain();
-  gainNode.connect(device.node);
-  device.node.connect(context.destination);
+  gainNode.connect(drumDevice.node);
+  drumDevice.node.connect(context.destination);
+  
+  gainNode.connect(synthDevice.node);
+  synthDevice.node.connect(context.destination);
+
   /*
   device.parameters.forEach(parameter => {
   console.log(parameter.id+": "+parameter.value);
@@ -36,19 +46,21 @@ async function startAudio()
 function triggerKick()
 {
   const event1 = new RNBO.MessageEvent(RNBO.TimeNow, "in1", RNBO.bang);
-  device.scheduleEvent(event1);
+  drumDevice.scheduleEvent(event1);
 }
 
 function triggerSnare()
 {
   const event1 = new RNBO.MessageEvent(RNBO.TimeNow, "in2", RNBO.bang);
-  device.scheduleEvent(event1);
+  drumDevice.scheduleEvent(event1);
 }
 
 function triggerHihat()
 {
   const event1 = new RNBO.MessageEvent(RNBO.TimeNow, "in3", RNBO.bang);
-  device.scheduleEvent(event1);
+  drumDevice.scheduleEvent(event1);
+  const event2 = new RNBO.MessageEvent(RNBO.TimeNow, "in1", RNBO.bang);
+  synthDevice.scheduleEvent(event2);
 }
   
 
